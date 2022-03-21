dfcfbc76b47e10d75a8e8db07365e5dbb4ac184a
Fatih Çelebi <57872210+celebiFatih@users.noreply.github.com>
2022-03-21 16:16:32 +0300

    Update Driver.java

--- src/mayintarlasi/Driver.java
+++ src/mayintarlasi/Driver.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mayintarlasi;
 
-/**
- *
- * @author casper
- */
 public class Driver {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {                
        BaslangicEkr baslat =new BaslangicEkr();
    }     
